f13dd8a1ae663e2767833bc2996d8ca4d2279406
Sciathlon <SciathlonBlog@gmail.com>
2018-01-21 13:26:03 +0100

    new data and analysis on personal bests

--- script_figure_skating.R
+++ script_figure_skating.R
@@ -1,1 +1,13 @@
 #analysis of stats on olympics figure skating
+
+#loading libraries
+library(ggplot2)
+
+#reading csv file
+medalCount <- read.csv(file="./stats_athletes_with_medals_count.csv", header=TRUE, sep=",")
+medalCountdf <- data.frame(medalCount)
+
+
+sum(medalCountdf$G)+sum(medalCountdf$S)+sum(medalCountdf$B)
+ggplot(data=medalCountdf, aes(x=NationID, y=(sum(medalCountdf$G)+sum(medalCountdf$S)+sum(medalCountdf$B)))) +
+  geom_bar(stat="identity")
